21533534c6eb0cb114e278b4ee9747d7e5ee495c
igor-tsap <tsap.ihor@gmail.com>
2025-10-29 11:16:06 +0200

    fix: filenames etc.

---
+++ pynative/Python_Basic_Exercise_for_Beginners/ex_1.py
@@ -0,0 +1,16 @@
+number1 = 20
+number2 = 30
+
+if (number1 * number2) < 1000:
+    print("The result is", number1 * number2)
+else:
+    print("The result is", number1 + number2)
+
+number1 = 40
+number2 = 30
+
+if (number1 * number2) < 1000:
+    print("The result is", number1 * number2)
+else:
+    print("The result is", number1 + number2)
+
